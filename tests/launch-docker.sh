6fc0d42cac1c8c65b4d16d6f387a905c169ef0ec
martbhell <martbhell+github@gmail.com>
2015-11-29 20:31:37 +0200

    expose it even more with a docker run -p

--- tests/launch-docker.sh
+++ tests/launch-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build --rm -t local/c7-systemd -f tests/Dockerfile .
-sudo docker run -it --privileged=true --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/ansible-role-squid:rw local/c7-systemd /bin/bash -c "/ansible-role-squid/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
+sudo docker run -p 3128 -it --privileged=true --rm=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v `pwd`:/ansible-role-squid:rw local/c7-systemd /bin/bash -c "/ansible-role-squid/tests/test-in-docker-image.sh ${OS_TYPE} ${OS_VERSION} ${ANSIBLE_VERSION}"
